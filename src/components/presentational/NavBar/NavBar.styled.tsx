ea8cc084836d7bc8281e6b1d1ffb920faa1dc5c0
NataGalvez <nataliac.galvezc@gmail.com>
2023-05-14 23:52:30 -0400

    chore: add background menu item selected

--- src/components/presentational/NavBar/NavBar.styled.tsx
+++ src/components/presentational/NavBar/NavBar.styled.tsx
@@ -1,16 +1,11 @@
 import { Menu, styled } from "@mui/material";
 import MenuItem from "@mui/material/MenuItem";
 
-interface Props {
-  selected:boolean
-}
-
 export const MenuStyled = styled(Menu)({
-    top:'5%',
-  });
-  export const MenuItemStyled = styled(MenuItem)((props:Props)=>({
-    '&..Mui-selected': {
-     backgroundColor: 'green'
-    },
-    
-  }));
+  top: "5%",
+});
+export const MenuItemStyled = styled(MenuItem)(() => ({
+  "&.Mui-selected": {
+    backgroundColor: "#43FF2D",
+  },
+}));
