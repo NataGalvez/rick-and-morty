796fceb4216039b63fc46f6c6e8eda8154fe0e3d
NataGalvez <nataliac.galvezc@gmail.com>
2023-05-15 09:51:03 -0400

    add base name to router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import './App.css'
-import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
-import Home from './components/presentational/Home/Home';
-import Navbar from './components/presentational/NavBar/NavBar';
-import PrincipalCharacters from './components/presentational/PrincipalCharacters/PrincipalCharacters';
-import Locations from './components/presentational/Locations/Locations';
-import Episodes from './components/presentational/Episodes/Episodes';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/presentational/Home/Home";
+import Navbar from "./components/presentational/NavBar/NavBar";
+import PrincipalCharacters from "./components/presentational/PrincipalCharacters/PrincipalCharacters";
+import Locations from "./components/presentational/Locations/Locations";
+import Episodes from "./components/presentational/Episodes/Episodes";
 
 function App() {
-
   return (
-    <Router>
-     <Navbar/>
-     <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/protagonistas" element={<PrincipalCharacters/>} />
-      <Route path="/lugares" element={<Locations/>} />
-      <Route path="/episodios" element={<Episodes/>} />
-            </Routes>
+    <Router basename={import.meta.env.DEV ? "/" : "/react-vite-gh-pages/"}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/protagonistas" element={<PrincipalCharacters />} />
+        <Route path="/lugares" element={<Locations />} />
+        <Route path="/episodios" element={<Episodes />} />
+      </Routes>
     </Router>
-   
-  )
+  );
 }
 
-export default App
-
+export default App;
